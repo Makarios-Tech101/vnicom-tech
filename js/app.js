fbc1e1bfd5fd1f055d815179cc8a5b6475981014
Ohien Stephen <ohienstephen@gmail.com>
2022-03-10 23:00:24 +0100

    -----------

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
 const toggle_btn = document.getElementById('toggle-nav')
 const close_btn = document.getElementById('close')
+const links = document.getElementsByClassName('close')
 const menu = document.getElementById('menu')
 
 toggle_btn.addEventListener('click', ()=>{
     menu.classList.add('show')
+})
+
+Array.from(links).forEach(link => {
+    link.addEventListener('click', ()=>{
+        menu.classList.remove('show')
+    } )
 })
 
 close_btn.addEventListener('click', ()=>{
