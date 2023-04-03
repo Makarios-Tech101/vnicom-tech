6108c8aaab4e465f9001f7700f07627c38ddb02a
unknown <ogunsanyaadewumi01@gmail.com>
2023-04-03 19:46:22 +0100

    added backend course page

--- js/app.js
+++ js/app.js
@@ -17,46 +17,23 @@ close_btn.addEventListener('click', ()=>{
     menu.classList.remove('show')
 })
 
-var btn1 = document.getElementById('btn1')
-var btn2 = document.getElementById('btn2')
-// var btn3 = document.getElementById('btn3')
-var view1 = document.getElementById('view1')
-var view2 = document.getElementById('view2')
-// var view3 = document.getElementById('view3')
 
+var btns = document.querySelectorAll('.oBtnNbg')
+var views = document.getElementsByClassName('view')
 
-
-var display = document.getElementById('display')
-
-btn1.addEventListener('click', ()=>{
-    btn1.classList.add('btn1')
-    btn2.style.background = ""
-    // btn3.style.background = ""
-
-    view1.style.display = "block"
-    view2.style.display = "none"
-    // view3.style.display = "none"
+btns[0].addEventListener('click', ()=>{
+    views[0].style.display = "block"
+    views[1].style.display = "none"
+    btns[0].classList.add('bg')
+    btns[1].classList.remove('bg')
     
 })
-
-btn2.addEventListener('click', ()=>{
-    btn1.classList.remove('btn1')
-    
-    btn2.style.background = "#ffbf00"
-    // btn3.style.background = ""
-    view2.style.display = "block"
-    // view3.style.display = "none"
-    view1.style.display = "none"
-
+btns[1].addEventListener('click', ()=>{
+    views[1].style.display = "block"
+    views[0].style.display = "none"
+    btns[1].classList.add('bg')
+    btns[0].classList.remove('bg')
 })
 
-// btn3.addEventListener('click', ()=>{
-//     btn1.style.background = ""
-//     btn2.style.background = ""
-//     btn3.style.background = "#ffbf00"
-//     view3.style.display = "block"
-//     view1.style.display = "none"
-//     view2.style.display = "none"
 
-
-// })
+    
