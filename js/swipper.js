59cd3855317a1f507e51b144a17b2d7b66acfef6
Adewumi <ogunsanyaadewumi01@gmail.com>
2024-05-20 01:23:45 +0100

    Adjusted student page design

--- js/swipper.js
+++ js/swipper.js
@@ -16,15 +16,15 @@
       },
       breakpoints: {
         640: {
-          slidesPerView: 3,
+          slidesPerView: 2,
           spaceBetween: 20,
         },
         768: {
-          slidesPerView: 3,
+          slidesPerView: 2,
           spaceBetween: 40,
         },
         1024: {
-          slidesPerView: 3,
+          slidesPerView: 2,
           spaceBetween: 50,
         },
       },
